2008d588a5ef7916e998c596236ba2df2810f898
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2023-08-29 19:38:39 +0200

    Cambio minimo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image(), tailwind(), sitemap(), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
